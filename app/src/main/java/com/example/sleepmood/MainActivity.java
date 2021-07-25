dc3922cf64481fd7d4e960d35f1a8fe15e0b0049
hominjang <51106030+hominjang@users.noreply.github.com>
2021-07-25 17:18:50 +0900

    210725 home_page complete / fragment_pages Add

--- app/src/main/java/com/example/sleepmood/MainActivity.java
+++ app/src/main/java/com/example/sleepmood/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.sleepmood;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // 21-07-23 하단 네비게이션 변수
+    BottomNavigationView bottomNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,20 +26,46 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences pref = getSharedPreferences("checkFirst", Activity.MODE_PRIVATE);
         boolean checkFirst = pref.getBoolean("checkFirst", false);
 
-        if(checkFirst==false){
+        if (checkFirst == false) {
             // @######구글 로그인 세션까지 조건 작업 할것######@
             // 앱 최초 실행시 하고 싶은 작업
             SharedPreferences.Editor editor = pref.edit();
-            editor.putBoolean("checkFirst",true);
+            editor.putBoolean("checkFirst", true);
             editor.commit();
 
             Intent intent = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(intent);
             finish();
-        }
-        else{
+        } else {
             // 최초 실행이 아닐때 진행할 작업
         }
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_frame, new Fragment_Home()).commit();
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) { //item을 클릭시 id값을 가져와 FrameLayout에 fragment.xml띄우기
+                    case R.id.page_home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_frame, new Fragment_Home()).commit();
+                        break;
+                    case R.id.page_record:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_frame, new Fragment_Record()).commit();
+                        break;
+                    case R.id.page_light:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_frame, new Fragment_light()).commit();
+                        break;
+                    case R.id.page_setting:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_frame, new Fragment_Setting()).commit();
+                        break;
+
+
+                    }
+                return true;
+            }
+        }
+    );
+
 
     }
 }
