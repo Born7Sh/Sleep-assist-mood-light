f01c3c4d3cf6a0fe38b8c8fed1b125bd0f5d7428
Inho Choi <born7sh@gmail.com>
2021-08-28 14:02:53 +0900

    V 1.2.0 (무드등 색 제어)

--- flask/app.py
+++ flask/app.py
@@ -3,10 +3,11 @@ import time
 import serial
 
 app = Flask(__name__)
-arduino = serial.Serial('/dev/ttyACM0',9600)
+#arduino = serial.Serial('/dev/ttyACM0',9600)
 
 @app.route('/temp')
 def temp():
+    arduino = serial.Serial('/dev/ttyACM0',9600)
     var = '0'
     humidity = 0
     temperature = 0
@@ -16,21 +17,141 @@ def temp():
 
     var = var.encode('utf-8')
     arduino.write(var)
-    for i in range (0, 4):
+    for i in range (0, 3):
         data = arduino.readline() 
         print(data)
-        if i == 3:    #온습도 센서가 2초 정도의 딜레이 후에 정상적으로 반응하는데 3번째 정도면 값을 받을 수 있음.
+        if i == 2:    #온습도 센서가 2초 정도의 딜레이 후에 정상적으로 반응하는데 3번째 정도면 값을 받을 수 있음.
             string = str(data)
-            string = string.split()
-            string[0] = string[0][2:]
-            string[2] = string[2][:-5]
+            string = string.split()     
+            string[0] = string[0][2:]   #쓰레기 값 제거
+            string[2] = string[2][:-5]  #쓰레기 값 제거
             print(string)
             humidity = string[0]
             temperature = string[1]
             lux = string[2]
-    print(temperature)
-    print(humidity)    
-    print(lux)  
+            print(temperature)
+            print(humidity)    
+            print(lux)  
+            arduino.close()
+    return 'ok'
+
+@app.route('/red')
+def red():
+    arduino = serial.Serial('/dev/ttyACM0',9600)
+    var = 'R'
+    var = var.encode('utf-8')
+    arduino.write(var)
+    data = arduino.readline() 
+    print(data)
+    arduino.close()
+    return 'ok'
+
+@app.route('/blue')
+def blue():
+    arduino = serial.Serial('/dev/ttyACM0',9600)
+    var = 'B'
+    var = var.encode('utf-8')
+    arduino.write(var)
+    arduino.close()
+    return 'ok'
+
+@app.route('/green')
+def green():
+    arduino = serial.Serial('/dev/ttyACM0',9600)
+    var = 'G'
+    var = var.encode('utf-8')
+    arduino.write(var)
+    arduino.close()
+    return 'ok'
+
+@app.route('/pink')
+def pink():
+    arduino = serial.Serial('/dev/ttyACM0',9600)
+    var = 'P'
+    var = var.encode('utf-8')
+    arduino.write(var)
+    arduino.close()
+    return 'ok'
+
+@app.route('/white')
+def white():
+    arduino = serial.Serial('/dev/ttyACM0',9600)
+    var = 'W'
+    var = var.encode('utf-8')
+    arduino.write(var)
+    arduino.close()
+    return 'ok'
+
+@app.route('/yellow')
+def yellow():
+    arduino = serial.Serial('/dev/ttyACM0',9600)
+    var = 'Y'
+    var = var.encode('utf-8')
+    arduino.write(var)
+    arduino.close()
+    return 'ok'
+
+@app.route('/sky')
+def sky():
+    arduino = serial.Serial('/dev/ttyACM0',9600)
+    var = 'S'
+    var = var.encode('utf-8')
+    arduino.write(var)
+    arduino.close()
+    return 'ok'
+
+@app.route('/rainbow')
+def rainbow():
+    arduino = serial.Serial('/dev/ttyACM0',9600)
+    var = 'A'
+    var = var.encode('utf-8')
+    arduino.write(var)
+    arduino.close()
+    return 'ok'
+
+@app.route('/black')
+def black():
+    arduino = serial.Serial('/dev/ttyACM0',9600)
+    var = 'L'
+    var = var.encode('utf-8')
+    arduino.write(var)
+    arduino.close()
+    return 'ok'
+
+@app.route('/poweron')
+def poweron():
+    arduino = serial.Serial('/dev/ttyACM0',9600)
+    var = 'O'
+    var = var.encode('utf-8')
+    arduino.write(var)
+    arduino.close()
+    return 'ok'
+
+@app.route('/poweroff')
+def poweroff():
+    arduino = serial.Serial('/dev/ttyACM0',9600)
+    var = 'F'
+    var = var.encode('utf-8')
+    arduino.write(var)
+    arduino.close()
+    return 'ok'
+
+@app.route('/brightup')
+def brightup():
+    arduino = serial.Serial('/dev/ttyACM0',9600)
+    var = 'U'
+    var = var.encode('utf-8')
+    arduino.write(var)
+    arduino.close()
+    return 'ok'
+
+@app.route('/brightdown')
+def brightdown():
+    arduino = serial.Serial('/dev/ttyACM0',9600)
+    var = 'D'
+    var = var.encode('utf-8')
+    arduino.write(var)
+    arduino.close()
     return 'ok'
 
 
--- hardware/temptest.ino
+++ hardware/temptest.ino
@@ -1,35 +1,137 @@
 #include <DHT.h>
 #include <Wire.h>
 #include <BH1750.h>
+#include <Adafruit_NeoPixel.h>
 #define DHTPIN A1
 #define DHTTYPE DHT11
 
+#define PIN 6
+
+//네오픽셀을 사용하기 위해 객체 하나를 생성한다. 
+//첫번째 인자값은 네오픽셀의 LED의 개수
+//두번째 인자값은 네오픽셀이 연결된 아두이노의 핀번호
+//세번째 인자값은 네오픽셀의 타입에 따라 바뀌는 flag
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(16, PIN, NEO_GRB + NEO_KHZ800);
+
 DHT dht(DHTPIN, DHTTYPE);
+
 BH1750 lightMeter;
+
 void setup(){
   Serial.begin(9600);
   Wire.begin();
   lightMeter.begin();
+  strip.begin(); //네오픽셀을 초기화하기 위해 모든LED를 off시킨다
+  strip.show(); 
 }
 
 
-int data,h,t;
+int data,h,t,bright;
 
 void loop(){
-  while(1){
+  while(Serial.available()){
     delay(2000);
+    
     data = Serial.read();
-    h = dht.readHumidity();
-    t = dht.readTemperature();
+    Serial.print(data);
+    if(data == 82){ //RED
+      colorWipe(strip.Color(255, 0, 0), 50);
+    }
+    if(data == 66){ //BLUE
+      colorWipe(strip.Color(0, 0, 255), 50);
+    }
+    if(data == 71){ //GREEN
+      colorWipe(strip.Color(0, 255, 0), 50);
+    }
+    if(data == 80){ //PINK
+      colorWipe(strip.Color(255, 0, 255), 50);
+    }
+    if(data == 87){ //WHITE
+      colorWipe(strip.Color(255, 255, 255), 50);
+    }
+    if(data == 89){ //YELLOW
+      colorWipe(strip.Color(255, 255, 0), 50);
+    }
+    if(data == 83){ //SKY
+      colorWipe(strip.Color(0, 255, 255), 50);
+    }
+    if(data == 65){ //RAINBOW
+      rainbow(20);
+    }
+    if(data == 76){ //BLACK
+      colorWipe(strip.Color(0, 0, 0), 50);
+    }
+    if(data == 79){ //POWER ON
+      colorWipe(strip.Color(255, 255, 0), 50);
+      strip.setBrightness(30);
+    }
+    if(data == 70){ //POWER OFF
+      strip.clear();
+      strip.show();
+    }
+    if(data == 85){ //Bright UP
+      strip.setBrightness(strip.getBrightness()+3);
+    }
+    if(data == 68){ //Bright DOWN
+      strip.setBrightness(strip.getBrightness()-3);
+    }
+    
+    h = dht.readHumidity();     //온도
+    t = dht.readTemperature();  //습도
     Serial.print(h);
     Serial.print(" ");
     Serial.print(t);
     Serial.print(" ");
-    int cdsValue = analogRead(A4);
+    int cdsValue = analogRead(A4);  
     int lux = lightMeter.readLightLevel();
-    Serial.println(lux);
+    Serial.println(lux);        //조도
+  }
+}
 
-    if(data=='1'){ 
+//모든 LED를 출력가능한 모든색으로 한번씩 보여주는 동작을 한번하는 함수
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
     }
+    strip.show();
+    delay(wait);
+  }
+}
+
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, c);
+      strip.show();
+      delay(wait);
+  }
+}
+
+//NeoPixel에 달린 LED를 각각 다른색으로 시작하여 다양한색으로 5번 반복한다
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { 
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+//255가지의 색을 나타내는 함수
+uint32_t Wheel(byte WheelPos) {
+  if(WheelPos < 85) {
+   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if(WheelPos < 170) {
+   WheelPos -= 85;
+   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
 }
