a1df4fdc2b81e7162d4423ee16943ea517b3d385
Born7sh <born7sh@gmail.com>
2021-09-20 21:28:29 +0900

    무드등 온도 말하기 기능 추가

--- hardware/test.py
+++ hardware/test.py
@@ -1,12 +1,12 @@
 import speech_recognition as sr
 import os
+import pymysql
+import config
+import datetime
 from mpyg321.mpyg321 import MPyg321Player
 from gtts import gTTS
 
-
-
 # 녹음을 3초동안 계속 반복하고 원하는 단어가 나오면 4초간 명령을 받아들임.
-
 
 while True:
     os.system('arecord -d 3 -f cd -Dhw:1 test.wav')
@@ -23,9 +23,9 @@ while True:
             tts = gTTS( text='부르셨어요?', lang='ko', slow=False )
             filename= 'ex_ko.mp3'
             tts.save(filename) 
-            os.system('sudo mpg321 -q '+filename)
+            os.system('sudo mpg321 -q '+filename)   #sudo 권한이 없으면 소리재생 불가
         
-            os.system('arecord -d 4 -f cd -Dhw:1 test.wav')
+            os.system('arecord -d 3 -f cd -Dhw:1 test.wav')
             r = sr.Recognizer()
 
             with sr.AudioFile("test.wav")as source:
@@ -38,6 +38,43 @@ while True:
                 filename= 'ex_ko.mp3'
                 tts.save(filename) 
                 os.system('sudo mpg321 -q '+filename)
+            if text == '날씨' or text == '오늘 날씨 알려줘':
+                # database = pymysql.connect(host=str(config.host),user=str(config.user),db=str(config.database),password=str(config.password), charset='utf8')
+                # curs=database.cursor()
+                # sql = '''SELECT * FROM current_weather ORDER BY id DESC LIMIT 11'''
+                # curs.execute(sql)
+                # result = cursor.fetchall()
+
+                database.commit()
+                database.close()    
+                tts = gTTS( text='안녕하세요. 반가워요.', lang='ko', slow=False )
+            if text.find('온도') != -1:
+                database = pymysql.connect(host=str(config.host),user=str(config.user),db=str(config.database),password=str(config.password), charset='utf8')
+                curs=database.cursor()
+                sql = '''SELECT temperature, humidity, bright FROM hardware_control_status where email = %s limit 1'''
+                curs.execute(sql,(config.email))
+                result = curs.fetchall()
+                voice_text = '현재 방 온도는 ' + str(int(result[0][0])) + '도, 습도는 ' +  str(result[0][1]) + ', 조도는 ' +  str(result[0][2]) + '입니다. '
+                database.commit()
+                database.close()    
+                tts = gTTS( text=voice_text, lang='ko', slow=False )
+                filename= 'temperature.mp3'
+                tts.save(filename) 
+                os.system('sudo mpg321 -q '+filename)
+            if text == '불 켜줘':
+                #기능
+                tts = gTTS( text='네 알겠습니다.', lang='ko', slow=False )
+                filename= 'ok.mp3'
+                tts.save(filename) 
+                os.system('sudo mpg321 -q '+filename)
+            if text == '불 꺼줘':
+                #기능
+                tts = gTTS( text='네 알겠습니다.', lang='ko', slow=False )
+                filename= 'ok.mp3'
+                tts.save(filename) 
+                os.system('sudo mpg321 -q '+filename)
+
+
     except:
         print('Canceled')        
  
