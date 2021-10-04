22e91f217f1a74da3241102b7f2ac3881f8f2c8a
Born7sh <born7sh@gmail.com>
2021-10-04 15:38:08 +0900

    sleep 모드 시 자동 불켜짐

--- mood/src/main/java/org/kpu/mood/persistence/UserDAO.java
+++ mood/src/main/java/org/kpu/mood/persistence/UserDAO.java
@@ -6,4 +6,5 @@ public interface UserDAO {
 	public UserVO readEmail(String email) throws Exception;
 	public void insertUser(UserVO userVO) throws Exception;
 	public void deleteUser(String email) throws Exception;
+	public String readIP(String email) throws Exception;
 }
